753542f4c6eaca9e3dbba096b07ba6eadf6fec7a
Shreyashree Banerjee <147500684+ShreyashreeRUKE@users.noreply.github.com>
2026-05-14 00:22:15 +0530

    Updated README and project files

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
 class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    ]
+
     title = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
+
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+        default='Medium'
+    )
+
+    due_date = models.DateField(null=True, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
